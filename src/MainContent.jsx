855bf7286330ea7a5e670c26ceb38e972b96156b
Uriel Ortiz <22001668@galileo.edu>
2025-01-23 23:02:26 -0600

    First challenges

---
+++ src/MainContent.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+const MainContent = () => {
+  return (
+    <>
+        <Header/>
+        <main>
+            <h2>Main Content</h2>
+            <p>This is the main content of the website. You can put any content you like in this area.</p>
+        </main>
+        <Footer/>
+    </>
+  )
+}
+
+export default MainContent
